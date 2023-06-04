c9d59209e57c2237abb22ad78347b684f63aa8e4
Bárbara Barrios Fulquet <barbarabarriosfulquet@gmail.com>
2023-06-04 19:38:30 -0400

    Add about-me component

--- ProyectoIntegrador_FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_FrontEnd/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { CertificationsCoursesComponent } from './components/certifications-courses/certifications-courses.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    CertificationsCoursesComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
