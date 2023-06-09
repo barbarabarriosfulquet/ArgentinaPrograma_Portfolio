700064bfe63f2ac019ab77c7d943a2fcdddabc82
Bárbara Barrios Fulquet <barbarabarriosfulquet@gmail.com>
2023-06-09 16:25:53 -0400

    Add login component

--- ProyectoIntegrador_FrontEnd/src/app/components/header/header.component.ts
+++ ProyectoIntegrador_FrontEnd/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { person } from 'src/app/model/person.model';
 import { PersonService } from 'src/app/service/person.service';
 
@@ -9,8 +10,11 @@ import { PersonService } from 'src/app/service/person.service';
 })
 export class HeaderComponent implements OnInit {
   person: person = new person("", "", "");
-  constructor(public personService: PersonService) { }
+  constructor(public personService: PersonService, private router: Router) { }
   ngOnInit(): void {
     this.personService.getPerson().subscribe(data => { this.person = data })
+  }
+  login() {
+    this.router.navigate(['/login'])
   }
 }
