0a4b7207e47b22f5cd67ba687d9bd5861947bac3
Bárbara Barrios Fulquet <barbarabarriosfulquet@gmail.com>
2023-06-15 17:14:58 -0400

    Add logout button

--- ProyectoIntegrador_FrontEnd/src/app/components/header/header.component.ts
+++ ProyectoIntegrador_FrontEnd/src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { person } from 'src/app/model/person.model';
 import { PersonService } from 'src/app/service/person.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-header',
@@ -9,11 +10,23 @@ import { PersonService } from 'src/app/service/person.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isLogged = false;
   person: person = new person("", "", "");
-  constructor(public personService: PersonService, private router: Router) { }
+  constructor(public personService: PersonService, private router: Router, private tokenService: TokenService) { }
   ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged=true;
+    }else{
+      this.isLogged = false;
+    }
     this.personService.getPerson().subscribe(data => { this.person = data })
   }
+
+  logout():void{
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+
   login() {
     this.router.navigate(['/login'])
   }
